f36767853adf283e5b253e322e8ad3da6fc419d1
Lorenzo Peña Cuevas <l.cuevas@mitur.gob.do>
2023-03-22 15:59:02 -0400

    FutureBuilder

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -13,36 +13,39 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  var _isLoading = false;
-
+  late Future _orderFuture;
+  Future _obtainOrdersFuture() =>
+      Provider.of<OrderServices>(context, listen: false).fetchAndSetOrder();
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<OrderServices>(context, listen: false)
-          .fetchAndSetOrder();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-
+    _orderFuture = _obtainOrdersFuture();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<OrderServices>(context);
+    // final orderData = Provider.of<OrderServices>(context);
     return Scaffold(
       appBar: AppBar(title: Text('Your orders')),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (context, index) =>
-                  OrderItem(orderData.orders[index]),
-            ),
+      body: FutureBuilder(
+          future: _orderFuture,
+          builder: (ctx, dataSnap) {
+            if (dataSnap.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (dataSnap.error != null) {
+              return Center(
+                child: Text("Error"),
+              );
+            } else {
+              return Consumer<OrderServices>(
+                builder: (ctx, ordetData, child) => ListView.builder(
+                  itemCount: ordetData.orders.length,
+                  itemBuilder: (context, index) =>
+                      OrderItem(ordetData.orders[index]),
+                ),
+              );
+            }
+          }),
       drawer: AppDrawer(),
     );
   }
