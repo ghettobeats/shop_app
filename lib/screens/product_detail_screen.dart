fa7292aff0a5a048eed647c66c313e21d5f48422
Lorenzo Peña Cuevas <l.cuevas@mitur.gob.do>
2023-04-11 10:50:48 -0400

    Slivers

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -16,45 +16,81 @@ class ProductDetailsScreen extends StatelessWidget {
     ).findById(productId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
-                tag: loadedProduct.id!,
-                child: Image.network(
-                  loadedProduct.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title),
+      // ),
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          pinned: true,
+          expandedHeight: 300,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(loadedProduct.title),
+            background: Hero(
+              tag: loadedProduct.id!,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '\$${loadedProduct.price}',
-              style: TextStyle(color: Colors.grey, fontSize: 20),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              width: double.infinity,
-              child: Text(
-                loadedProduct.description,
-                textAlign: TextAlign.center,
-                softWrap: true,
-              ),
-            )
-          ],
+          ),
         ),
-      ),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          const SizedBox(height: 10),
+          Text(
+            '\$${loadedProduct.price}',
+            style: const TextStyle(color: Colors.grey, fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            width: double.infinity,
+            child: Text(
+              loadedProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          ),
+          const SizedBox(height: 800),
+        ]))
+      ]),
+      // body: SingleChildScrollView(
+      //   child: Column(
+      //     children: [
+      //       Container(
+      //         height: 300,
+      //         width: double.infinity,
+      //         child: Hero(
+      //           tag: loadedProduct.id!,
+      //           child: Image.network(
+      //             loadedProduct.imageUrl,
+      //             fit: BoxFit.cover,
+      //           ),
+      //         ),
+      //       ),
+      //       SizedBox(
+      //         height: 10,
+      //       ),
+      //       Text(
+      //         '\$${loadedProduct.price}',
+      //         style: TextStyle(color: Colors.grey, fontSize: 20),
+      //       ),
+      //       SizedBox(
+      //         height: 10,
+      //       ),
+      //       Container(
+      //         padding: EdgeInsets.symmetric(horizontal: 10),
+      //         width: double.infinity,
+      //         child: Text(
+      //           loadedProduct.description,
+      //           textAlign: TextAlign.center,
+      //           softWrap: true,
+      //         ),
+      //       )
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
